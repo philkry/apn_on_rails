de9f728c5967bc10bb1e297eeaa97b1fa306b3c3
philkry <philkry@gmx.de>
2012-05-06 17:06:57 +0200

    removed custom_properties from json

--- lib/apn_on_rails/app/models/apn/notification.rb
+++ lib/apn_on_rails/app/models/apn/notification.rb
@@ -58,11 +58,6 @@ class APN::Notification < ActiveRecord::Base
       result['aps']['sound'] = self.sound if self.sound.is_a? String
       result['aps']['sound'] = "1.aiff" if self.sound.is_a?(TrueClass)
     end
-    if self.custom_properties
-      self.custom_properties.each do |key,value|
-        result["#{key}"] = "#{value}"
-      end
-    end
     result
   end
   
